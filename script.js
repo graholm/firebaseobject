8071259bb61ee282490dc69baebe859335b31fab
graholm <graholm@nuevaschool.org>
2016-10-21 08:48:03 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ var newDiv = $("<div></div>")
   var nameHeader = $("<h1 class='product-name'>" + product.name + "</h1>")
   var image = $("<img class='product-image'>").attr("src", "http://pix.iemoji.com/images/emoji/apple/ios-9/256/thinking-face.png")  
 
-  $(".product-list-container").append(div,nameHeader, image)
+  $(".product-list-container").append(newDiv, nameHeader, image)
 })
