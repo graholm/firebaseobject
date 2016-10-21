7198b7e8944f02390039476944d9c16dc163482b
graholm <graholm@nuevaschool.org>
2016-10-21 08:51:54 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 //addListItem("product-list", {name: "cool product"})
 
 onNewListItem("product-list", function(product) {
-  console.log(product)
-var newDiv = $("<div>"+product.price+"</div>") // <div>10</div>
+  console.log(product.image)
+  
+var newDiv = $("<div>"+product.price+"</div>")
   var nameHeader = $("<h1 class='product-name'>" + product.name + "</h1>")
   var image = $("<img class='product-image'>").attr("src", "http://pix.iemoji.com/images/emoji/apple/ios-9/256/thinking-face.png")  
+  
 
   $(".product-list-container").append(newDiv, nameHeader, image)
 })
